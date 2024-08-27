3828706a1a4daf5a92ee0d20fbf0296dfbfa4530
Ilakkiya <ilakkiya.s2022cse@sece.ac.in>
2024-08-27 22:18:45 +0530

    Research

--- page.py
+++ page.py
@@ -46,7 +46,7 @@ def apply_custom_css():
 def main_page():
     st.title("Ewing Sarcoma: A Comprehensive Overview")
     st.write("Ewing sarcoma is a rare and aggressive type of cancer that primarily affects the bones or the soft tissue surrounding the bones (soft tissue sarcoma). It is most commonly diagnosed in children, teenagers, and young adults, but it can occur at any age.")
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 6.04.21 PM.jpeg", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 6.04.21 PM.jpeg", use_column_width=True)
 # Function to display Content1 page
 def content1_page():
     st.title("Ewing Sarcoma: A Comprehensive Overview")
@@ -62,7 +62,7 @@ def content1_page():
     st.markdown("""
     Ewing sarcoma is characterized by the presence of a specific chromosomal translocation, typically between chromosomes 11 and 22, resulting in the EWSR1-FLI1 fusion gene. This abnormal gene leads to uncontrolled cell growth and contributes to the malignancy of the tumor.
     """, unsafe_allow_html=True)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.07.45 PM.jpeg", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.07.45 PM.jpeg", use_column_width=True)
     st.markdown("<h2>Common Sites of Occurrence:</h2>", unsafe_allow_html=True)
     st.markdown("""
     - **Bone**: The most common sites include the pelvis, femur (thigh bone), tibia (shin bone), and humerus (upper arm bone).
@@ -87,7 +87,7 @@ def content1_page():
         - A definitive diagnosis is made by biopsy, where a sample of the tumor is examined under a microscope.
         - Immunohistochemistry and molecular testing (such as fluorescence in situ hybridization, or FISH) are used to detect the EWSR1-FLI1 fusion gene.
     """, unsafe_allow_html=True)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.11.04 PM.jpeg", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.11.04 PM.jpeg", use_column_width=True)
 
     st.markdown("<h2>Staging:</h2>", unsafe_allow_html=True)
     st.markdown("""
@@ -101,9 +101,9 @@ def content1_page():
         - **Chemotherapy**: Preoperative (neoadjuvant) chemotherapy to shrink the tumor and postoperative (adjuvant) chemotherapy to eliminate any remaining cancer cells.
                 
                 """)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.10.07 PM.jpeg", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.10.07 PM.jpeg", use_column_width=True)
     st.markdown("""- **Surgery**: Removal of the tumor with a margin of healthy tissue. Limb-sparing surgery is preferred, but amputation may be necessary in some cases.""")
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.15.43 PM.jpeg", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.15.43 PM.jpeg", use_column_width=True)
     st.markdown("""- **Radiation Therapy**: Used when complete surgical resection is not possible or in cases of metastasis. It can also be used as part of the initial treatment.
     - **Targeted Therapy**:
         - **Clinical Trials**: Ongoing research into targeted therapies that specifically attack cancer cells with the EWSR1-FLI1 fusion gene.
@@ -136,7 +136,7 @@ def content2_page():
     The primary causative factor in Ewing sarcoma is the EWS-FLI1 fusion protein. This abnormal protein results from a chromosomal translocation between chromosomes 11 and 22, specifically t(11;22)(q24;q12). This translocation fuses the EWSR1 gene (Ewing sarcoma breakpoint region 1) on chromosome 22 with the FLI1 gene (Friend leukemia integration 1 transcription factor) on chromosome 11.
     """, unsafe_allow_html=True)
 
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.44.16 PM.jpeg ", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.44.16 PM.jpeg", use_column_width=True)
 
 # Function to display Content3 page (Sequence)
 def content3_page():
@@ -152,8 +152,8 @@ FLI1 Domain: The FLI1 portion includes an ETS (E-twenty-six) DNA-binding domain 
 
 The sequence is not fixed, as different variants of the fusion gene can occur, leading to different fusion protein sequences. However, the most common variant includes the first seven exons of the EWSR1 gene fused to exons 6-9 of the FLI1 gene.
     """, unsafe_allow_html=True)
-    # st.image(" E:\mallene_project\WhatsApp Image 2024-08-27 at 7.44.41 PM (3).jpeg", use_column_width=True)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.44.41 PM (3).jpeg ", use_column_width=True)
+    
+    st.image("WhatsApp Image 2024-08-27 at 7.44.41 PM (3).jpeg ", use_column_width=True)
 
 # Function to display Content4 page (Inhibitors)
 def content4_page():
@@ -166,14 +166,14 @@ def content4_page():
 Mechanism: A small molecule that targets and inhibits the function of the EWS-FLI1 protein by disrupting its interaction with RNA helicase A, which is essential for its oncogenic activity.
 Clinical Status: TK216 has shown promise in early-phase clinical trials, particularly in patients with relapsed or refractory Ewing sarcoma.
 """)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.21.51 PM.jpeg ", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.21.51 PM.jpeg", use_column_width=True)
     st.markdown("""
 **YK-4-279:**
 
 Mechanism: An inhibitor that disrupts the interaction between EWS-FLI1 and RNA helicase A, thereby reducing the oncogenic activity of the fusion protein. YK-4-279 also induces apoptosis in Ewing sarcoma cells.
 Clinical Status: YK-4-279 is still in preclinical and early clinical stages, but it has shown potential as a targeted therapy.
 """)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.37.26 PM.jpeg ", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.37.26 PM.jpeg ", use_column_width=True)
     st.markdown("""
 **Trabectedin:**
 
@@ -190,7 +190,7 @@ Clinical Status: HSP90 inhibitors are being investigated in preclinical models a
 Mechanism: The EWS-FLI1 protein upregulates the expression of anti-apoptotic proteins like BCL-2. Inhibitors of BCL-2, such as venetoclax, have shown potential in sensitizing Ewing sarcoma cells to apoptosis.
 Clinical Status: BCL-2 inhibitors are under investigation for use in combination with other therapies in Ewing sarcoma.
     """, unsafe_allow_html=True)
-    st.image("E:\mallene_project\WhatsApp Image 2024-08-27 at 7.43.07 PM.jpeg ", use_column_width=True)
+    st.image("WhatsApp Image 2024-08-27 at 7.43.07 PM.jpeg", use_column_width=True)
 
 
 # Page navigation logic
